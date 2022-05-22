3217f49e8ceca777bf2ce54a2a0fe3154098626e
Sumeet Chhetri <sumeet.chhetri@gmail.com>
2022-05-22 12:12:01 +0530

    Removed tests and server related code Simplified cmake file to build for any compatible posix platform This required zookeeper_mt to be prebuilt and available separately

--- .github/ISSUE_TEMPLATE.md
+++
@@ -1,3 +0,0 @@
-Don't forget the label!
-
- - If you have a question, use the "question" tag.
--- src/zk/tests/main.cpp
+++
@@ -1,7 +0,0 @@
-#include "test.hpp"
-
-int main(int argc, char** argv)
-{
-    ::testing::InitGoogleTest(&argc, argv);
-    return ::RUN_ALL_TESTS();
-}
